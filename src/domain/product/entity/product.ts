89df6b580eef06c315336538d0079ebf525116d5
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2022-10-03 14:09:28 -0300

    Desafio Validação de Products

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,5 +1,6 @@
 import Entity from "../../@shared/entity/entity.abstract";
 import NotificationError from "../../@shared/notification/notification.error";
+import ProductValidatorFactory from "../factory/product.validator.factory";
 import ProductInterface from "./product.interface";
 
 export default class Product extends Entity implements ProductInterface {
@@ -42,15 +43,16 @@ export default class Product extends Entity implements ProductInterface {
   }
 
   validate(): boolean {
-    if (this._id.length === 0) {
-      this.notification.addError({ context: "Product", message: "Id is required" });
-    }
-    if (this._name.length === 0) {
-      this.notification.addError({ context: "Product", message: "Name is required" });
-    }
-    if (this._price < 0) {
-      this.notification.addError({ context: "Product", message: "Price must be greater than zero" });
-    }
+    // if (this._id.length === 0) {
+    //   this.notification.addError({ context: "Product", message: "Id is required" });
+    // }
+    // if (this._name.length === 0) {
+    //   this.notification.addError({ context: "Product", message: "Name is required" });
+    // }
+    // if (this._price < 0) {
+    //   this.notification.addError({ context: "Product", message: "Price must be greater than zero" });
+    // }
+    ProductValidatorFactory.create().validate(this);
     return !this.notification.hasErrors();
   }
 }
